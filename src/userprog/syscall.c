8b38233703c66013fa9ecc29d2725ba02fba8681
Adil Sheikh <adilzs@hazelnut.rlogin>
2022-02-15 09:10:25 -0500

    added the function definitions and the switch statement

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,8 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include <filesys/file.h>
+#include <filesys/filesys.h>
 
 static void syscall_handler (struct intr_frame *);
 
@@ -33,12 +35,160 @@ static void
 syscall_handler (struct intr_frame *f) 
 {
   // printf ("system call!\n");
-  // int sys_call_number = (f->esp + 1);
+  int sys_call_number = *((uint8_t*)(f->esp + 1));
 
-  // switch (0) {
-  //   case SYS_HALT: {
-  //     break;
-  //   }
-  // }
+  switch (sys_call_number)
+    {
+      case SYS_HALT:
+        {
+          break;
+        }
+      case SYS_EXIT:
+        {
+          break;
+        }
+      case SYS_EXEC:
+        {
+          break;
+        }
+      case SYS_WAIT:
+        {
+          break;
+        }
+      case SYS_CREATE:
+        {
+          break;
+        }
+      case SYS_REMOVE:
+        {
+          break;
+        }
+      case SYS_OPEN:
+        {
+          break;
+        }
+      case SYS_FILESIZE:
+        {
+          break;
+        }
+      case SYS_READ:
+        {
+          break;
+        }
+      case SYS_WRITE:
+        {
+          break;
+        }
+      case SYS_SEEK:
+        {
+          break;
+        }
+      case SYS_TELL:
+        {
+          break;
+        }
+      case SYS_CLOSE:
+        {
+          break;
+        }
+    }
   thread_exit ();
+}
+
+/*  */
+static void
+check_user_args(void *arg)
+{
+  return 0;
+}
+
+/*  */
+static void
+sys_halt(void)
+{
+  return 0;
+}
+
+/*  */
+static void
+sys_exit(int status)
+{
+  return 0;
+}
+
+/*  */
+static pid_t
+sys_exec(const char *cmd_line)
+{
+  return 0;
+}
+
+/*  */
+static int
+sys_wait(pid_t pid)
+{
+  return 0;
+}
+
+/*  */
+static bool
+sys_create(const char *file, unsigned initial_size)
+{
+  return 0;
+}
+
+/*  */
+static bool
+sys_remove(const char *file)
+{
+  return 0;
+}
+
+/*  */
+static int
+sys_open(const char* file)
+{
+  return 0;
+}
+
+/*  */
+static int
+sys_filesize(int fd)
+{
+  return 0;
+}
+
+/*  */
+static int
+sys_read(int fd, void *buffer, unsigned size)
+{
+  return 0;
+}
+
+/*  */
+static int
+sys_write(int fd, const void *buffer, unsigned size)
+{
+  return 0;
+}
+
+/*  */
+static void
+sys_seek(int fd, unsigned position)
+{
+  return 0;
+}
+
+/*  */
+static unsigned
+sys_tell(int fd)
+{
+  return 0;
+}
+
+/*  */
+static void
+sys_close(int fd)
+{
+  return 0;
 }
