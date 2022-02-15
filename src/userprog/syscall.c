d2c32f6c2738e42b58eaf6c38ce37a8c73ce3909
Adil Sheikh <adilzs@beech.rlogin>
2022-02-14 21:45:37 -0500

    starting the next project, made a couple of changes to get started.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,23 @@
 
 static void syscall_handler (struct intr_frame *);
 
+/* Our Code */
+static void check_user_args(void *arg);
+
+static void sys_halt(void);
+static void sys_exit(int status);
+static pid_t sys_exec(const char *cmd_line);
+static int sys_wait(pid_t pid);
+static bool sys_create(const char *file, unsigned initial_size);
+static bool sys_remove(const char *file);
+static int sys_open(const char* file);
+static int sys_filesize(int fd);
+static int sys_read(int fd, void *buffer, unsigned size);
+static int sys_write(int fd, const void *buffer, unsigned size);
+static void sys_seek(int fd, unsigned position);
+static unsigned sys_tell(int fd);
+static void sys_close(int fd);
+
 void
 syscall_init (void) 
 {
@@ -13,8 +30,15 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
+  // printf ("system call!\n");
+  // int sys_call_number = (f->esp + 1);
+
+  // switch (0) {
+  //   case SYS_HALT: {
+  //     break;
+  //   }
+  // }
   thread_exit ();
 }
